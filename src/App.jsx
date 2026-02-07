d23c334a3587039d8358ece31e054daf29854e59
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-07 21:59:12 +0530

    react router dom added and used

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 //called from main.jsx
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   )
 }
 
