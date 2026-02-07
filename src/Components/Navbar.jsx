fab5eeb4df81866cc4b4a0ece7e985f1133f4277
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-07 22:04:02 +0530

    work on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { navbarStyles } from "../assets/dummyStyle";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={navbarStyles.nav}>
+      <div
+        style={{ backgroundImage: navbarStyles.decorativePatternBackground }}
+        className={navbarStyles.decorativePattern}
+      ></div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
