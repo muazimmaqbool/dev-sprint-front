ffe3ad8ef09827f12b198fd15301286b3f8bb1aa
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-08 17:00:07 +0530

    login form designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import Login from './Pages/Login'
 
 //called from main.jsx
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
   )
 }
