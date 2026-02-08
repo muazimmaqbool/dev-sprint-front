2dbfb0b004ec67a75018fc7f5c2a393c9b5dce70
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-08 11:29:20 +0530

    added react icons and basic styles to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import { navbarStyles } from "../assets/dummyStyle";
+import { Link, NavLink } from "react-router-dom";
+import { FiAward } from "react-icons/fi";
 
-const Navbar = () => {
+const Navbar = ({ logoSrc }) => {
   return (
     <nav className={navbarStyles.nav}>
       <div
         style={{ backgroundImage: navbarStyles.decorativePatternBackground }}
         className={navbarStyles.decorativePattern}
       ></div>
+
+      <div className={navbarStyles.bubble1}></div>
+      <div className={navbarStyles.bubble2}></div>
+      <div className={navbarStyles.bubble3}></div>
+
+      <div className={navbarStyles.container}>
+        <div className={navbarStyles.logoContainer}>
+          <Link to="/" className={navbarStyles.logoButton}>
+            <div className={navbarStyles.logoInner}>
+              <img
+                src={
+                  logoSrc ||
+                  "https://yt3.googleusercontent.com/eD5QJD-9uS--ekQcA-kDTCu1ZO4d7d7BTKLIVH-EySZtDVw3JZcc-bHHDOMvxys92F7rD8Kgfg=s900-c-k-c0x00ffffff-no-rj"
+                }
+                alt="QuizMaster logo"
+                className={navbarStyles.logoImage}
+              />
+            </div>
+          </Link>
+        </div>
+
+        <div className={navbarStyles.titleContainer}>
+          <div className={navbarStyles.titleBackground}>
+            <h1 className={navbarStyles.titleText}>Dev Sprint</h1>
+          </div>
+        </div>
+
+        <div className={navbarStyles.desktopButtonsContainer}>
+            <div className={navbarStyles.spacer}></div>
+            <NavLink to={"/result"} className={navbarStyles.resultsButton}>
+                <FiAward className={navbarStyles.buttonIcon}/>
+                My Result
+            </NavLink>
+        </div>
+
+      </div>
     </nav>
   );
 };
