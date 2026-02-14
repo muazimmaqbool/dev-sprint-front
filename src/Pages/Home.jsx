d32ed216d2556bbd4cb5a6916390d6ded45fe9e9
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-14 14:37:19 +0530

    side bar file added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import SideBar from "../Components/SideBar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <SideBar/>
     </div>
   );
 };
