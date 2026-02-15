9dcee27ae102d7dc830a01bd41adab16896b0b16
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-15 15:40:37 +0530

    red line on unused variables removed

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { sidebarStyles } from '../assets/dummyStyle'
+import questionsData from '../assets/questions'
 
 const SideBar = () => {
+     const [selectedTech, setSelectedTech] = useState(null);
+  const [selectedLevel, setSelectedLevel] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState({});
+  const [showResults, setShowResults] = useState(false);
+
+  const submittedRef = useRef(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const asideRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) setIsSidebarOpen(true);
+      else setIsSidebarOpen(false);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>SideBar</div>
   )
