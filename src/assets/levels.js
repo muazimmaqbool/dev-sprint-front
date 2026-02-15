fcdf12a9f386439e5af4a2d03ca522d37643273d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-15 15:49:54 +0530

    icons to levels imported and added

--- src/assets/levels.js
+++ src/assets/levels.js
@@ -1,23 +1,26 @@
+import { FaRegStar } from "react-icons/fa";
+import { FiTarget, FiZap } from "react-icons/fi";
+
 export   const levels = [
     {
       id: "basic",
       name: "Basic",
       questions: 20,
-      icon: <Star size={16} />,
+      icon: <FaRegStar size={16} />,
       color: "bg-green-50 text-green-600",
     },
     {
       id: "intermediate",
       name: "Intermediate",
       questions: 40,
-      icon: <Zap size={16} />,
+      icon: <FiZap size={16} />,
       color: "bg-blue-50 text-blue-600",
     },
     {
       id: "advanced",
       name: "Advanced",
       questions: 60,
-      icon: <Target size={16} />,
+      icon: <FiTarget size={16} />,
       color: "bg-purple-50 text-purple-600",
     },
   ];
